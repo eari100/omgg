2ce94c503c3d6280f2d5ef3461d2e0a9c578049b
Jaewook Choi <eari100@naver.com>
2021-01-02 21:28:46 +0900

    Initial commit

---
+++ src/main/java/gg/om/omgg/OmggApplication.java
@@ -0,0 +1,13 @@
+package gg.om.omgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmggApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OmggApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gg/om/omgg/OmggApplicationTests.java
@@ -0,0 +1,13 @@
+package gg.om.omgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmggApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
