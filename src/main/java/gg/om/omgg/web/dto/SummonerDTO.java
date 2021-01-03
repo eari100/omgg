8e038ffc5b2fecb6f3586fc71f46757e2e7a3ad3
Jaewook Choi <eari100@naver.com>
2021-01-03 23:28:52 +0900

    test: lombok 적용 테스트 - SummonerDTO 생성 - ombok 어노테이션 적용 및 테스트

---
+++ src/main/java/gg/om/omgg/web/dto/SummonerDTO.java
@@ -0,0 +1,15 @@
+package gg.om.omgg.web.dto;
+
+import lombok.*;
+
+@Getter
+@RequiredArgsConstructor
+public class SummonerDTO {
+    private final String accountId;
+    private final int profileIconId;
+    private final long revisionDate;
+    private final String name;
+    private final String id;
+    private final String puuid;
+    private final long summonerLevel;
+}
---
+++ src/test/java/gg/om/omgg/web/dto/SummonerDTOTests.java
@@ -0,0 +1,28 @@
+package gg.om.omgg.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SummonerDTOTests {
+
+    @Test
+    public void 롬복_적용_테스트() {
+        String accountId = "yy15F-qXoM8a1kqFL8iJ0xMUTF6e6ZZlWKPdlrvgZIcr";
+        int profileIconId = 11;
+        long revisionDate = 1609294136;
+        String name = "거세짱123";
+        String id = "qOshc-BI3WAaQuvgpPI7GY7w0ZfjTt2WJHX_46zdQVqotlI";
+        String puuid = "blugvIvgoZB2GPmLQryiiVl_61CnLNNf50b_UGKkCqilTFa42mL_ZEfSEUJTICP_X-n6xuMjMg65YQ";
+        long summonerLevel = 293;
+
+        SummonerDTO dto = new SummonerDTO(accountId, profileIconId, revisionDate, name, id, puuid, summonerLevel);
+
+        assertThat(dto.getAccountId()).isEqualTo(accountId);
+        assertThat(dto.getProfileIconId()).isEqualTo(profileIconId);
+        assertThat(dto.getRevisionDate()).isEqualTo(revisionDate);
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getId()).isEqualTo(id);
+        assertThat(dto.getPuuid()).isEqualTo(puuid);
+        assertThat(dto.getSummonerLevel()).isEqualTo(summonerLevel);
+    }
+}
