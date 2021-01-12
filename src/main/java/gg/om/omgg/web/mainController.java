4f0be3114b098e55bf09c60211b1913ad28dc2f2
Jaewook Choi <eari100@naver.com>
2021-01-12 16:55:26 +0900

    feat: summonerDetail.html 호출하는 메서드 추가

--- src/main/java/gg/om/omgg/web/mainController.java
+++ src/main/java/gg/om/omgg/web/mainController.java
@@ -2,11 +2,17 @@ package gg.om.omgg.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class mainController {
     @GetMapping("/")
     public String main() {
         return "main";
+    }
+
+    @GetMapping("/userName={name}")
+    public String selectDetail(@PathVariable("name") String name) {
+        return "summonerDetail";
     }
 }
