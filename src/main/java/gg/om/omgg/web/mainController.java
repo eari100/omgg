a4e5b8acf1d274a20554e1e897bca469f43d6494
Jaewook Choi <eari100@naver.com>
2021-01-13 09:56:23 +0900

    refactor: 소환사 정보 호출 로직 RiotApiController -> mainController로 옮김

--- src/main/java/gg/om/omgg/web/mainController.java
+++ src/main/java/gg/om/omgg/web/mainController.java
@@ -1,18 +1,33 @@
 package gg.om.omgg.web;
 
+import gg.om.omgg.api.riot.service.SummonerService;
+import gg.om.omgg.dto.SummonerResponseDTO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Optional;
+
+@RequiredArgsConstructor
 @Controller
 public class mainController {
+
+    private final SummonerService summonerService;
+
     @GetMapping("/")
     public String main() {
         return "main";
     }
 
     @GetMapping("/userName={name}")
-    public String selectDetail(@PathVariable("name") String name) {
+    public String selectDetail(Model model, @PathVariable("name") String summonerName) {
+
+        Optional<SummonerResponseDTO> dto = summonerService.findByName(summonerName);
+
+        model.addAttribute("summoner", dto);
+
         return "summonerDetail";
     }
 }
