1843c44d705a7b75209ec9ac100857a022b5c81f
Jaewook Choi <eari100@naver.com>
2021-01-24 12:53:38 +0900

    feat : summoner 갱신 기능 추가에 따른 summonerResponseDTO에 id 컬럼 추가

--- src/main/java/gg/om/omgg/web/dto/SummonerResponseDTO.java
+++ src/main/java/gg/om/omgg/web/dto/SummonerResponseDTO.java
@@ -8,10 +8,12 @@ public class SummonerResponseDTO {
     private int profileIconId;
     private String name;
     private long summonerLevel;
+    private String id;
 
     public SummonerResponseDTO(Summoner entity) {
         this.profileIconId = entity.getProfileIconId();
         this.name = entity.getName();
         this.summonerLevel = entity.getSummonerLevel();
+        this.id = entity.getId();
     }
 }
