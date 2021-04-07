5b47a47eeeaf14a9d4f031d737511066d347362b
Jaewook Choi <eari100@naver.com>
2021-04-07 23:37:23 +0900

    test : domain summoner, match  저장 후 summoner_match에도 저장되는 지 테스트

--- src/test/java/gg/om/omgg/domain/summoner/SummonerRepositoryTests.java
+++ src/test/java/gg/om/omgg/domain/summoner/SummonerRepositoryTests.java
@@ -1,5 +1,8 @@
 package gg.om.omgg.domain.summoner;
 
+import gg.om.omgg.api.riot.service.SummonerService;
+import gg.om.omgg.domain.match.Match;
+import gg.om.omgg.domain.match.MatchRepository;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,6 +22,9 @@ public class SummonerRepositoryTests {
 
     @Autowired
     SummonerRepository summonerRepository;
+
+    @Autowired
+    MatchRepository matchRepository;
 
     @After
     public void cleanUp() {
@@ -115,5 +122,49 @@ public class SummonerRepositoryTests {
 
         Optional<Summoner> summoner = summonerRepository.findById(id);
         assertThat(summoner.isEmpty()).isEqualTo(true);
+    }
+
+    @Test
+    public void summoner이랑match에_저장하면_summoner_match에도_저장되는지_테스트() {
+        String accountId = "yy15F-qXoM8a1kqFL8iJ0xMUTF6e6ZZlWKPdlrvgZIcr";
+        int profileIconId = 11;
+        long revisionDate = 1609294136000L;
+        String name = "거세짱123";
+        String id = "qOshc-BI3WAaQuvgpPI7GY7w0ZfjTt2WJHX_46zdQVqotlI";
+        String puuid = "blugvIvgoZB2GPmLQryiiVl_61CnLNNf50b_UGKkCqilTFa42mL_ZEfSEUJTICP_X-n6xuMjMg65YQ";
+        long summonerLevel = 293;
+
+        Summoner summoner = Summoner.builder()
+                .accountId(accountId)
+                .profileIconId(profileIconId)
+                .revisionDate(revisionDate)
+                .name(name)
+                .id(id)
+                .puuid(puuid)
+                .summonerLevel(summonerLevel)
+                .build();
+
+        long[] gameIds = {5115327873L, 5114612211L, 5114509070L, 5113679956L, 5113381568L,
+                5113123898L, 5113140714L, 5113009318L, 5113093150L, 5113040393L,
+                5112862930L, 5112801333L, 5111953625L, 5111855885L, 5111560760L,
+                5111318269L, 5110948806L, 5110712716L, 5110647401L, 5110663852L};
+
+        Arrays.sort(gameIds);
+        int[] queueIds = {420, 450, 420, 420, 420, 450, 450, 450, 450, 450, 420, 450, 420, 450, 450, 450, 420, 450, 450, 450};
+        String platformId = "KR";
+        int seasonId = 13;
+
+        for(int i=0;i<20;i++) {
+            Match match = matchRepository.save(Match.builder()
+                    .gameId(gameIds[i])
+                    .queueId(queueIds[i])
+                    .platformId(platformId)
+                    .seasonId(seasonId)
+                    .build()
+            );
+            // 연관관계 추가
+            summoner.getMatches().add(match);
+        }
+        summonerRepository.save(summoner);
     }
 }
