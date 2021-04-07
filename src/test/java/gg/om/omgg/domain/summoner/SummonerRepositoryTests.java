f40659525065c24e2920196763c72c19da3702bd
Jaewook Choi <eari100@naver.com>
2021-04-07 12:14:29 +0900

    test : 안쓰는 import 제거

--- src/test/java/gg/om/omgg/domain/summoner/SummonerRepositoryTests.java
+++ src/test/java/gg/om/omgg/domain/summoner/SummonerRepositoryTests.java
@@ -1,6 +1,5 @@
 package gg.om.omgg.domain.summoner;
 
-import gg.om.omgg.web.dto.SummonerIntegrationInformationResponseDTO;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
