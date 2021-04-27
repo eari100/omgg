e38a619e6dfcc7f30c028467e1093339336c353e
Jaewook Choi <eari100@naver.com>
2021-04-27 12:41:58 +0900

    refactor : 소환사 통합 정보 검색을 위해서 Match 와 Participant 연관관계 Match -> Participant로 수정

--- src/main/java/gg/om/omgg/domain/match/Match.java
+++ src/main/java/gg/om/omgg/domain/match/Match.java
@@ -1,10 +1,14 @@
 package gg.om.omgg.domain.match;
 
+import gg.om.omgg.domain.participant.Participant;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @NoArgsConstructor
 @Entity
@@ -17,6 +21,10 @@ public class Match {
     private int seasonId;
     private long gameCreation;
     private long gameDuration;
+
+    @OneToMany
+    @JoinColumn(name = "game_id", referencedColumnName = "game_id")
+    List<Participant> participants = new ArrayList<>();
 
     @Builder
     public Match(long gameId, int queueId, String platformId, int seasonId, long gameCreation, long gameDuration) {
--- src/main/java/gg/om/omgg/domain/participant/ParticipantId.java
+++ src/main/java/gg/om/omgg/domain/participant/ParticipantId.java
@@ -1,27 +1,24 @@
 package gg.om.omgg.domain.participant;
 
-import gg.om.omgg.domain.match.Match;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import java.io.Serializable;
 
 @Getter
 @Embeddable
 @NoArgsConstructor
 public class ParticipantId implements Serializable {
-    @ManyToOne
-    @JoinColumn(name = "game_id")
-    private Match match;
+    @Column(name="game_id")
+    private long gameId;
     private int participantId;
 
     @Builder
-    public ParticipantId(Match match, int participantId) {
-        this.match = match;
+    public ParticipantId(long gameId, int participantId) {
+        this.gameId = gameId;
         this.participantId = participantId;
     }
 }
