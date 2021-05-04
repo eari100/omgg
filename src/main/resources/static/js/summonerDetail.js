84fd39ba47fdc7e89afc6b5af7a5db56a9d948a3
Jaewook Choi <eari100@naver.com>
2021-05-04 16:46:37 +0900

    feat : 더 보기 버튼 누를 시 해당 api 호출하도록 추가

--- src/main/resources/static/js/summonerDetail.js
+++ src/main/resources/static/js/summonerDetail.js
@@ -4,6 +4,10 @@ var main = {
         $('#btn-refresh').on('click', function() {
             _this.renewData()
         })
+
+        $('#btn-gameMore').on('click', function() {
+            _this.gameMore()
+        })
     },
     renewData : function() {
         var data = {
@@ -17,9 +21,35 @@ var main = {
             contentType:'application/json; charset=utf-8',
             data: JSON.stringify(data)
         }).done(function() {
-            window.location.href = '/userName='+data.name;
-        }).fail(function (error) {
-            alert(JSON.stringify(error));
+            window.location.href = '/userName='+data.name
+        }).fail(function(error) {
+            alert(JSON.stringify(error))
+        })
+    },
+    gameMore : function() {
+        var data = {
+            accountId: 'yy15F-qXoM8a1kqFL8iJ0xMUTF6e6ZZlWKPdlrvgZIcr', // 임시 데이터
+            name: $('#summonerName').val(),
+            endIndex: 400  // 임시 데이터
+        }
+
+        $.ajax({
+            type: 'POST',
+            url: '/api/matchesLeadMore',
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8',
+            data: JSON.stringify(data),
+            success: function (attr) {
+                console.log(attr)
+                alert('api를 불러오는 데 성공하였습니다. 콘솔 창을 확인해보세요.')
+            },
+            error: function (e) {
+                alert('매치 기록을 더 가져오는 도중 문제가 있었습니다. 잠시 후 시도해주세요.')
+            }
+        }).done(function() {
+            window.location.href = '#'
+        }).fail(function(error) {
+            alert(JSON.stringify(error))
         })
     }
 }
