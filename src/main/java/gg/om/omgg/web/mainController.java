45767b448ff53a8ddcfb67f5c290136b44ffec35
Jaewook Choi <eari100@naver.com>
2021-05-11 22:38:06 +0900

    feat : riot product key 심사 준비

--- src/main/java/gg/om/omgg/web/mainController.java
+++ src/main/java/gg/om/omgg/web/mainController.java
@@ -3,11 +3,16 @@ package gg.om.omgg.web;
 import gg.om.omgg.api.riot.service.SummonerService;
 import gg.om.omgg.web.dto.IntegrationInfoResponseDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -28,5 +33,17 @@ public class mainController {
         model.addAttribute("IntegrationInfo", dto);
 
         return "summonerDetail";
+    }
+
+    @GetMapping("/static/riot.txt")
+    public void riotAPIKeyAccess() {
+        ClassPathResource resource = new ClassPathResource("static/riot.txt");
+        try {
+            Path path = Paths.get(resource.getURI());
+            List<String> content = Files.readAllLines(path);
+            content.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
